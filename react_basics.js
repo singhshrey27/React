478da639031fb2f90215dd78a875170415daeb1c
shiransh <sp999pratap@gmail.com>
2023-04-13 02:42:39 +0530

    folder added

---
+++ react_basics.js
@@ -0,0 +1,34 @@
+//Open source library of Javascript
+//Focus on UI
+//Rich ecosystem
+
+//Why Learn React?
+//1.Created and maintained by facebook having large community and 
+//2.Huge community
+//3.On demand skill
+
+//Based on component architecture
+//Code is reusable of each component
+//Tell react what you want and React will build the actural UI
+//React will handle efficiently updating and rendering of the components.
+//Dom updates are handles gracefull in React.
+//React Native for Mobile application
+
+//Prerequisites: 
+//1.Html,css and Javascript
+//2.ES6 (A modern javascript)
+
+//Requirements:
+//Javascript: 'this' keyword,filter,map and reduce
+//ES6 : let & const,arrow functions,template literals,default parameters,object literals,rest and spread operators and destructuring assignment
+
+//Roadmap:
+//1.Fundamentals
+//2.HTTP
+//3.Routing
+//4.Redux
+//5.Rest utilities and framework
+
+//npx create-react-app project-name => it creates a devlopment atmosphere in the browser
+//npx is pakage runner installed along with node it is the first method to use it directly with command
+//2nd method is npm approach (it installs the package globally here we need to update it constantly)
